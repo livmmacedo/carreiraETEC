2d31dbbb1b42d3064b4f53e8781a94cda0e8c22c
livmmacedo <livia.medeiros2@etec.sp.gov.br>
2023-09-12 13:01:15 -0300

    manda

---
+++ pages/empresa/empresa_controller.php
@@ -0,0 +1,20 @@
+<?
+
+class EmpresaController {
+
+    function incluir(Empresa $empresa){
+
+    }
+    function listar() {
+
+    }
+    function mostrar($id) {
+        
+    }
+    function atualizar(Empresa $empresa) {
+        
+    }
+    function deletar($id) {
+        
+    }
+}
