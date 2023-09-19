dd1c01c66d45a676677ddd13e901e5d49a7688ac
livmmacedo <livia.medeiros2@etec.sp.gov.br>
2023-09-19 14:18:24 -0300

    .

--- pages/empresa/index.php
+++ pages/empresa/index.php
@@ -1,12 +1,14 @@
 <?php
-  require_once 'empresa_controller.php';
+require_once 'empresa_controller.php';
 
-  //cria um controlador
-  $controlador = new EmpresaController(); 
 
-  //obtem a lista de empresas do banco de dados.
-  $lista = $controlador->listar();
 
+//cria um controlador
+$controlador = new EmpresaController();
+
+//obtem a lista de empresas do banco de dados.
+$lista = $controlador->listar();
+*/
 ?>
 <nav class="navbar sticky-top bg-body-tertiary">
   <div class="container-fluid">
@@ -17,27 +19,32 @@
 
 
 <div class="container-fluid">
-    <table class="table"> 
-          <thead> 
-               <tr>
-                 <th scope="col">Nome da Empresa</th>
-                 <th scope="col">Telefone</th>
-               </tr> 
-          </thead>  
-          <tbody>
-            <?php
-              foreach($lista as $empresa) {
-            ?>
-             <tr>
-                <td><a href="#"><?=$empresa->getNome()?></a></td>
-                <td ><?=$empresa->getTelefone()?></td>
-             </tr>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Nome da Empresa</th>
+        <th scope="col">Telefone</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      foreach ($lista as $empresa) {
+        ?>
+        <tr>
+          <td><a href="#"><?= $empresa->getNome() ?></a></td>
+          <td><?= $empresa->getTelefone() ?></td>
+        </tr>
 
-            <?php
-              }
-            ?>
+        <?php
+      }
+      ?>
 
 
-          </tbody>  
-    </table>    
-</div>    
+    </tbody>
+  </table>
+
+  <div id="div-add">
+    <i class="fa fa-plus-square" aria-hidden="true"></i>
+  </div>
+</div>
+
