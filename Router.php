aa10184aebae0473c7fcd02a89703dc0ec6675a8
livmmacedo <livia.medeiros2@etec.sp.gov.br>
2023-10-31 13:00:56 -0300

    manda

--- Router.php
+++ Router.php
@@ -4,21 +4,23 @@ require_once __DIR__ . '/RouteSwitch.php';
 
 class Router extends RouterSwitch
 {
-    
+
     public function run(string $requestUri)
     {
         $route = substr($requestUri, 1);
-        $route = str_replace("carreiraETEC/", "", $route);
+        $route = str_replace("carreira/", "", $route);
 
         error_log("Rota requisitada: $route",0);
-        $parts = explode("/",$route); //querando requisição
+        $parts = explode("/",$route); // quebrando a requisição
         error_log("Partes da requisição: ".print_r($parts,true),0);
 
-        $page = $parts[0];
-        $operation = count($parts)>1?$parts[1]:null;
 
-       
+        // obtenho a pagina
+        $page = $parts[0]; 
 
+        // obtenho a operação se for solicitada
+        $operation = count($parts)>1?$parts[1]:null; 
+        
         if ($route == '') {
             $this->home();
         } else {
