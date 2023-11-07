55a5948ba7827ebad90b42f85623a28eb119ec41
livmmacedo <livia.medeiros2@etec.sp.gov.br>
2023-11-07 12:47:35 -0300

    manda

--- pages/empresa/index.php
+++ pages/empresa/index.php
@@ -1,37 +1,38 @@
+
 <nav class="navbar sticky-top bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Lista de Empresas</a>
   </div>
 </nav>
 
-
-
 <div class="container-fluid">
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Nome da Empresa</th>
-        <th scope="col">Telefone</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      
-      foreach($controller->lista as $empresa) {
-      
-        ?>
-        <tr>
-          <td><a href="#"><?= $empresa->getNome() ?></a></td>
-          <td><?= $empresa->getTelefone() ?></td>
-        </tr>
+    <table class="table"> 
+          <thead> 
+               <tr>
+                 <th scope="col">Nome da Empresa</th>
+                 <th scope="col">Telefone</th>
+               </tr> 
+          </thead>  
+          <tbody>
+            <?php
+              foreach($empresas as $empresa) {
+            ?>
+             <tr>
+                <td><a href="#"><?=$empresa->getNome()?></a></td>
+                <td ><?=$empresa->getTelefone()?></td>
+             </tr>
 
-        <?php
-      }
-      ?>
+            <?php
+              }
+            ?>
+          </tbody>  
+    </table>    
 
 
-    </tbody>
-  </table>
+    <div id="div-add">
+       <a href="empresa/novo"><i class="fa fa-plus" aria-hidden="true"></i></a>
+    </div>  
+</div>    
 
   <div id="div-add">
    <a href="empresa/novo"> <i class="fa fa-plus-square" aria-hidden="true"></i>
