3eb00ca3b3ad7cfef08749edbecf6e9de7ad1aa4
livmmacedo <livia.medeiros2@etec.sp.gov.br>
2023-11-14 14:09:06 -0300

    manda

--- pages/empresa/novo.php
+++ pages/empresa/novo.php
@@ -1,45 +1,66 @@
 <div class="container">
-  
-    <form>
-        <div class="mb-3 input-group">
+ 
+    <form method="POST" id="formulario"> <!--02 Alterar o método da requisição para POST -->
+        <div class="mb-3 input-group has-validation">
           <span class="input-group-text">Nome</span>
           <!--Incluir Atributo "name" nos inputs que vão ser enviados para salvar no banco de dados -->        
           <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome da empresa">
+          <div class="invalid-feedback">
+            Por favor digite o nome da empresa
+          </div>
         </div>
-        
-        
+       
+       
+       <!--01- TELEFONE DA EMPRESA -->
+       <div class="mb-3 input-group has-validation "> <!--04 incluir a classe de campo com validação has-validation-->
+         <span class="input-group-text">Telefone</span>
+         <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite o telefone">
+         <!--03 informe uma mensagem de validação do campo telefone -->
+         <div class="invalid-feedback">
+          Por favor digite um telefone
+         </div>
+         <!--03 FIM informe uma mensagem de validação do campo telefone-->
+       </div>
+       <!--01-Fim TELEFONE da empresa-->
+ 
         <div class="mb-3 input-group">
         <span class="input-group-text">Estado</span>
         <select class="form-select" aria-label="Default select example" id="estados" onchange="carregarCidadesIBGE()">
             <option selected>Selecione um estado</option>
         </select>
+        <div class="invalid-feedback">
+          Por favor digite um Estado
+         </div>
         </div>            
-
+ 
         <div class="mb-3 input-group">
         <span class="input-group-text">Cidade</span>
         <select class="form-select" aria-label="Default select example" id="cidade" name="cidade">
-            <option selected>Selecione uma cidade</option>
+            <option selected>Selecione antes um estado</option>
         </select>
+        <div class="invalid-feedback">
+          Por favor selecione uma cidade
+         </div>
         </div>            
-
-
-
-        <div class="mb-3 input-group">
-            
-            <!--Incluir os inputs que serão utilizados para enviar a geolocalização  -->        
+ 
+ 
+ 
+        <div class="mb-3 input-group">      
             <input type="hidden" id="geolocalizacao" name="geolocalizacao">
-
             <span class="input-group-text">Endereço</span>
             <input type="text" class="form-control" id="endereco" placeholder="Digite o endereço da empresa">
             <button type="button"  onclick="obterCoordenadasGoogleMaps()" class="input-group-text">Localizar</button>
+            <div class="invalid-feedback">
+               Por favor localize a empresa no mapa.
+           </div>
         </div>
         <?php require 'pages/empresa/mapa.php'; ?>
-
+ 
         <!--botão para salvar o formulário -->        
         <div class="mb-3 input-group  justify-content-end ">
            <button type="submit"  class="input-group-text ">Salvar</button>
         </div>            
-        
+       
     </form>    
 </div>
 <script>
@@ -49,29 +70,29 @@
             .then(response => response.json())
             .then(data => {
                     if (data!=null && data.length>0) {
-
-                        
+ 
+                       
                         var selectEstado = document.getElementById("estados");
                         data.forEach(element => {
-                            
+                           
                             let option = document.createElement('option');
                             option.value= element['sigla'];
                             option.innerText = element['nome'];
                             selectEstado.appendChild(option);
-
+ 
                         });
-
-
+ 
+ 
                     }
             }).catch(error => {
                     console.log("Erro carregando estados "+error);
                 });;
-     } 
-
-
+     }
+ 
+ 
      carregarEstadosIBGE();
-
-
+ 
+ 
      function carregarCidadesIBGE(){
         var selectEstado = document.getElementById("estados");
         var estado = selectEstado.value;
@@ -81,21 +102,54 @@
             .then(data => {
                     if (data!=null && data.length>0) {
                         var selectCidade = document.getElementById("cidade");
-
-                        
+ 
+                       
                         selectCidade.innerHTML = "";
-                        
+                       
                         data.forEach(element => {
                             let option = document.createElement('option');
                             option.value= element['id'];
                             option.innerText = element['nome'];
                             selectCidade.appendChild(option);
-                            
+                           
                         });
                     }
             }).catch(error => {
                     console.log("Erro carregando cidades "+error);
             });;
-        
+       
      }
+ 
+     function validacao(e){
+        let passou = false;
+        let enome =document.getElementById("nome");
+        if (e.nome.value==null || enome.value == "") {
+            passou = false;
+            enome.classList.add("is-invalid");
+        } else {
+            enome.classList.remove("is-invalid");
+        }
+ 
+        let etelefone =document.getElementById("telefone");
+        if (etelefone.value==null || etelefone.value.length >=9) {
+            passou = false;
+            etelefone.classList.add("is-invalid");
+        } else {
+            etelefone.classList.remove("is-invalid");
+        }
+       
+        let ecidade =document.getElementById("cidade");
+        if (ecidade.value==null || ecidade.value.length >=9) {
+            passou = false;
+            ecidade.classList.add("is-invalid");
+        } else {
+            ecidade.classList.remove("is-invalid");
+        }
+ 
+        if (!passou) {
+             e.preventDefault();
+        }
+     }
+     document.getElementById("formulario").addEventListener('subimt',validacao);
 </script>    
+ 
